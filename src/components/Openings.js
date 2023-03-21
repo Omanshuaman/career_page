0aa3d0d1dd5d2ba85fe8b7d304b9819adc0088cc
Omanshu Aman <omanshuaman@gmail.com>
2023-03-21 18:15:58 +0530

    first commit

---
+++ src/components/Openings.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Openings = () => {
+  return <div>Openings</div>;
+};
+
+export default Openings;
---
+++ src/pages/Career.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import JoinUs from "../components/JoinUs";
+import Openings from "../components/Openings";
+
+function Career() {
+  return (
+    <div>
+      <Header />
+      <JoinUs />
+      <Openings />
+    </div>
+  );
+}
+
+export default Career;
