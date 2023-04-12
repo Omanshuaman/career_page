bba8d2680559293e7f38f6457350dfc765e5e554
Omanshu Aman <omanshuaman@gmail.com>
2023-04-12 11:19:04 +0530

    bug

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
-import AWS from "aws-sdk";
 import axios from "axios";
-import Modal from "./Modal";
-import Popup from "reactjs-popup";
-import "reactjs-popup/dist/index.css";
+import { v4 as uuidv4 } from "uuid";
 const Banner = ({ filteredApplicants, showSnackbar }) => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -11,18 +8,16 @@ const Banner = ({ filteredApplicants, showSnackbar }) => {
   const [resume, setResume] = useState(null);
   const [contactError, setContactError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-
+  const [base64, setBase64] = useState("");
+  const [textPosted, setTextPosted] = useState("");
   const now = new Date();
   const appliedDate = now.toISOString();
-
   const handleNameChange = (event) => {
     setName(event.target.value);
   };
-
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
-
   const handleContactChange = (event) => {
     const value = event.target.value;
     if (value.length <= 10) {
@@ -35,22 +30,37 @@ const Banner = ({ filteredApplicants, showSnackbar }) => {
   const handleResumeChange = (event) => {
     setResume(event.target.files[0]);
   };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     setIsLoading(true);
 
     try {
-      const formData = new FormData();
-      formData.append("demo file", resume, resume.name);
+      const file = resume;
+      const reader = new FileReader();
+
+      const uploadFile = new Promise((resolve, reject) => {
+        reader.onload = () => {
+          const base64 = reader.result;
+          const formData = new FormData();
+          formData.append("demo file", base64);
+
+          resolve(formData);
+        };
+        reader.onerror = reject;
+
+        reader.readAsDataURL(file);
+      });
+
+      const formData = await uploadFile;
 
       const uploadResponse = await axios.post(
-        "https://3pg8ch1ir2.execute-api.us-east-1.amazonaws.com/prod/file-upload",
+        "https://gsrhol3xd0.execute-api.ap-south-1.amazonaws.com/prod/file-upload",
         formData
       );
-
       const uploadedFileUrl = uploadResponse.data.fileUrl;
-
       const newApplicant = {
+        Id: uuidv4(),
         name: name,
         emailid: email,
         contactNumber: contact,
@@ -58,22 +68,21 @@ const Banner = ({ filteredApplicants, showSnackbar }) => {
         applied_date: appliedDate,
         job_id: filteredApplicants.job_id,
       };
-
       const applicantResponse = await axios.post(
-        "https://3pg8ch1ir2.execute-api.us-east-1.amazonaws.com/prod/applicant",
+        "https://gsrhol3xd0.execute-api.ap-south-1.amazonaws.com/prod/applicant",
         newApplicant
       );
-
+      setTextPosted("Suucessfully Uploaded");
+      // Refresh the page after 5 seconds
       setTimeout(() => {
         window.location.reload();
-      }, 1000);
+      }, 5000);
     } catch (err) {
       console.log(err);
     } finally {
       setIsLoading(false);
     }
   };
-
   const [disabled, setdisabled] = useState(false);
   const date = new Date();
   const formattedDate = `${date.getDate().toString().padStart(2, "0")}-${(
@@ -84,7 +93,6 @@ const Banner = ({ filteredApplicants, showSnackbar }) => {
   const date1 = formattedDate;
   const date2 = filteredApplicants.deadline;
   const myInputRef = useRef(null);
-
   useEffect(() => {
     if (myInputRef.current) {
       myInputRef.current.focus();
@@ -95,16 +103,18 @@ const Banner = ({ filteredApplicants, showSnackbar }) => {
   useEffect(() => {
     if (new Date(formattedDate1) > new Date(formattedDate2)) {
       setdisabled(true);
-      console.log("first");
     }
   }, []);
-
+  const [alertVisible, setAlertVisible] = useState(true);
+  const closeAlert = () => {
+    setAlertVisible(false);
+  };
   return (
     <div className="banner">
       <div className="banner-column">
         <div className="banner-text1">{filteredApplicants.role}</div>
         <div className="banner-text2">
-          Job Type: {filteredApplicants.job_type} | No of Vacancies :
+          Job Type: {filteredApplicants.job_type} | No of Vacancies:&nbsp;
           {filteredApplicants.number_of_vacancy}
         </div>
         <button
@@ -112,8 +122,7 @@ const Banner = ({ filteredApplicants, showSnackbar }) => {
           type="button"
           data-toggle="modal"
           data-target="#exampleModalLong"
-          disabled={disabled}
-        >
+          disabled={disabled}>
           Apply Now
         </button>
       </div>
@@ -123,8 +132,7 @@ const Banner = ({ filteredApplicants, showSnackbar }) => {
         tabindex="-1"
         role="dialog"
         aria-labelledby="exampleModalLongTitle"
-        aria-hidden="true"
-      >
+        aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
@@ -132,8 +140,7 @@ const Banner = ({ filteredApplicants, showSnackbar }) => {
                 type="button"
                 class="close"
                 data-dismiss="modal"
-                aria-label="Close"
-              >
+                aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
@@ -143,8 +150,7 @@ const Banner = ({ filteredApplicants, showSnackbar }) => {
                   <form
                     className="card-form"
                     encType="multipart/form-data"
-                    onSubmit={handleSubmit}
-                  >
+                    onSubmit={handleSubmit}>
                     <div className="name">Name</div>
                     <input
                       type="text"
@@ -161,9 +167,10 @@ const Banner = ({ filteredApplicants, showSnackbar }) => {
                     />
                     <div className="name">Contact Number:</div>
                     <input
-                      type="number"
+                      type="tel"
                       className="name-input"
                       required
+                      pattern="[0-9]{10}"
                       onChange={handleContactChange}
                     />
 
@@ -183,11 +190,11 @@ const Banner = ({ filteredApplicants, showSnackbar }) => {
                     <button
                       className="action-button"
                       type="submit"
-                      disabled={isLoading}
-                    >
+                      disabled={isLoading}>
                       {isLoading ? "Uploading..." : "Upload"}
                     </button>
                   </form>
+                  <h4>{textPosted}</h4>
                 </div>
               </div>
             </div>
@@ -197,5 +204,4 @@ const Banner = ({ filteredApplicants, showSnackbar }) => {
     </div>
   );
 };
-
 export default Banner;
